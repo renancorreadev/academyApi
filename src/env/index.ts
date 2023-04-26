d7fac4e5dc655228a23df857f68b5c1a98b9e545
Renan Correa <developer@skynance.net>
2023-04-25 23:42:57 -0300

    chore: install eslint

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,19 +1,17 @@
-import 'dotenv/config';
-import { z } from 'zod';
-
+import 'dotenv/config'
+import { z } from 'zod'
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
-  PORT: z.coerce.number().default(3333),
-});
-
+  PORT: z.coerce.number().default(3333)
+})
 
 const _env = envSchema.safeParse(process.env)
 
-if(_env.success === false) {
-  console.log('Invalid enviroment variables', _env.error.format());
+if (!_env.success) {
+  console.log('Invalid enviroment variables', _env.error.format())
 
-  throw new Error('Invalid environment variables');
+  throw new Error('Invalid environment variables')
 }
 
-export const env = _env.data;
+export const env = _env.data
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import {app} from './app';
-import {env} from './env';
+import { app } from './app'
+import { env } from './env'
 
-app.listen({
-    port: env.PORT,
-    host: '0.0.0.0'
+void app.listen({
+  port: env.PORT,
+  host: '0.0.0.0'
 }).then(() => {
   console.log('HTTP server is Running!! 🚀')
 })
