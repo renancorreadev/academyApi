ed3ffb263d1eee7c2a4169a82d9383d5a045da09
Renan Correa <developer@skynance.net>
2023-04-28 11:13:03 -0300

    refactor: change service register

---
+++ src/services/register.ts
@@ -0,0 +1,35 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
+
+interface RegisterServiceParams {
+  name: string
+  email: string
+  password: string
+}
+
+export async function registerService ({
+  name,
+  email,
+  password
+}: RegisterServiceParams): Promise<any> {
+  const password_hash = await hash(password, 6)
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email
+    }
+  })
+
+  if (userWithSameEmail != null) {
+    throw new Error('Email already in use')
+  }
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash
+    }
+  })
+}
