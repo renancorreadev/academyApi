3f61a6bc8a580e3477069caf5a6768a999ff332e
Renan Correa <developer@skynance.net>
2023-04-28 13:08:34 -0300

    fix: rename import User type

--- src/repository/prisma/prisma-users-repository.ts
+++ src/repository/prisma/prisma-users-repository.ts
@@ -1,14 +1,6 @@
 import { prisma } from '@/lib/prisma'
-import { type Prisma } from '@prisma/client'
+import { type Prisma, type User } from '@prisma/client'
 import { type UsersRepository } from './users-repository'
-
-interface User {
-  id: string
-  name: string
-  email: string
-  password_hash: string | null
-  created_at: Date
-}
 
 export class PrismaUserRepository implements UsersRepository {
   async findByEmail (email: string): Promise<User | null> {
